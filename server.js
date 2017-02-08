133afb3ea9a657f4749e1a16735f75e75367bef3
deeparsh98 <deep.bhargav98@gmail.com>
2017-02-08 22:21:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req,res){
-   res.send('THE ARTICLE ONE IS REQUESTED ...AND WILL BE SENT..!!');
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 } );
 
 app.get('/article-two', function(req,res){
