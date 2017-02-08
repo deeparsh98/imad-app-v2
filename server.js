19b2483b6474be92e36cb6fec972af7699c7f1c6
deeparsh98 <deep.bhargav98@gmail.com>
2017-02-08 23:23:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,63 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
+
+
+var articleOne={
+    title: 'Article 1 | Arsh Bhargav',
+  
+    content: ` <p>
+                    This is the content for my first article ...I have nothing to write here...
+                </p>
+            
+                <p>
+                    This is the content for my first article ...I have nothing to write here...
+                </p>
+            
+                <p>
+                    This is the content for my first article ...I have nothing to write here...
+                </p>
+            `,
+    date : 'Feb 08,2017',
+    heading : 'Article One'
+};
+function createTemplate(data)
+{
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    var htmlTemplate=`
+                       <html>
+                            <head>
+                                <title>
+                                    ${title}
+                                </title>
+                                <meta  name="viewport" contents="width=device-width, initial scale=1" />
+                                <link href="/ui/style.css" rel="stylesheet" />
+                            </head>
+                            <body>
+                                <div class="container">
+                                    
+                                    <div>
+                                        <a href="/"> HOME </a>
+                                    </div>
+                                    <hr/>
+                                    <h3>
+                                        ${heading}
+                                    </h3>
+                                    <div>
+                                        ${date}
+                                    </div>
+                                    <div>
+                                        ${content}
+                                    </div>
+                                </div>
+                            </body>
+                        </html>
+                        `;
+    return htmlTemplate;
+}
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
@@ -10,7 +67,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/ui/style.css', function (req, res) {
