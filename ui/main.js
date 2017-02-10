a2c3516a52c8197a0a1933b87f7fd3eebbc9549e
deeparsh98 <deep.bhargav98@gmail.com>
2017-02-10 20:35:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var element=document.getElementById('madi');
 
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+1;
     
     madi.style.marginLeft=marginLeft+'px';
 }
