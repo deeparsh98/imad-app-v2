aac2aa1018bd33d51494b80bf79229b8653d6076
deeparsh98 <deep.bhargav98@gmail.com>
2017-02-10 23:06:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ submit.onclick = function(){
       if(request.readyState === XMLHttpRequest.DONE){
           if(request.status === 200){
                var names=request.response;
-               names=JSON.parce(names);
+               names=JSON.parse(names);
     
                 var list='';
                 for(var i=0;i<names.length;i++)
