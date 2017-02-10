6ff19bc5003b5935c5a2ff32544aa0dcfbd674a3
deeparsh98 <deep.bhargav98@gmail.com>
2017-02-10 21:54:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,18 @@
 var button= document.getElementById('counter');
 
-var cnter=0;
-
 button.onclick = function(){
+    var request= new XMLhttpRequest();
     
+    request.onreadystatechange= function(){
+      if(request.readyState === XMLhttpRequest.DONE){
+          if(request.status === 200){
+                cnter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML= cnter.toString();
+          }
+      }
+    };
     
-  cnter=cnter+1;
-  var span=document.getElementById('count');
-  span.innerHTML= cnter.toString();
+  request.open('GET','http://deeparsh98.imad.hasura-app.io/counter', true);
+  request.send(null);
 };
