2dea327880f77ceb6e72e10f540b07fba8386f17
deeparsh98 <deep.bhargav98@gmail.com>
2017-02-11 16:40:08 +0530

    [imad-console] Updates ui/com.js

--- ui/com.js
+++ ui/com.js
@@ -1,11 +1,29 @@
 
-var comment=document.getElementById('com');
-comment=comment.value;
+
 
 var getCom=document.getElementById('getcom');
 
 getCom.onclick = function(){
   var request= new XMLHttpRequest();
-  
+ 
+ request.onreadystatechange = function(){
+   if(request.readyState=== XMLHttprequest.DONE){
+       if(request.status===200){
+           var comments= JSON.parse(comments);
+           var list='';
+           for(var i=0;i<comments.length;i++)
+           {
+               list+='<li>'+comments[i]+'</li>';
+           }
+           
+           var ol=document.getElementById('olist');
+           olist.innerHTML=list;
+       }
+   }  
+ };
     
 };
+var comment=document.getElementById('com');
+comment=comment.value;
+request.open('GET','http://deeparsh98@.imad.hasura-app.io/a1/comments?='+ comment, true);
+request.send(null);
