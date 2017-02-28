561711715ccb769b0965d50c2a637bc087415e8f
deeparsh98 <deep.bhargav98@gmail.com>
2017-02-28 23:03:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ pool.query('select *from test',function(err,result){
     if(err)
     res.status(500).send(err.toString());
     else{
-        res.send(JSON.stringify(result));
+        res.send(JSON.stringify(result.rows));
     }
 });
 
