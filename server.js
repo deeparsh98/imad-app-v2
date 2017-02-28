e7278d98c362477397b54bd8ffe9253af3da8a95
deeparsh98 <deep.bhargav98@gmail.com>
2017-02-28 23:01:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
+var config={
+    user: 'deeparsh98',
+    database: 'deeparsh98',
+    host: 'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+    
+};
 var app = express();
 
 
@@ -91,6 +100,19 @@ app.get('/counter', function(req,res){
     res.send(counter.toString());
 });
 
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+pool.query('select *from test',function(err,result){
+    if(err)
+    res.status(500).send(err.toString());
+    else{
+        res.send(JSON.stringify(result));
+    }
+});
+
+    
+});
 
 
 var comments=[];
