295c1ae1dbd2391bc2902e4c770f2850f566898f
deeparsh98 <deep.bhargav98@gmail.com>
2017-02-28 23:24:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.get('/submit-name', function(req, res){
 app.get('/articles/:articleName', function (req, res) {
     var articleName=req.params.articleName;
     
-    pool.query("select * from article where title="+articleName, function(err,result){
+    pool.query("select * from article where title= '"+articleName +"'", function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }
